cb61ed7183c77fe57826df1c2426b027fda62b1f
quanghoa <hoa9x3@gmail.com>
2018-09-17 22:50:55 +0700

    update code for menuside bar

---
+++ application/views/admin/lends/index.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <?php echo $pagetitle; ?>
+                    <?php echo $breadcrumb; ?>
+                </section>
+
+                <section class="content">
+                    <?php echo "lendings"; ?>
+                </section>
+            </div>
