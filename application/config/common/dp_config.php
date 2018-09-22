6a1172ba3d57be806fdb378d5b4f3ff04a1f705c
quanghoa <hoa9x3@gmail.com>
2018-09-22 13:50:16 +0700

    add templete for user side

--- application/config/common/dp_config.php
+++ application/config/common/dp_config.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // Possible hosts locally. You can add some if needed.
-$config['host_dev'] = array('localhost', '127.0.0.1', '::1');
+$config['host_dev'] = array('localhost', '127.0.0.1');
 
 // Fill in the file of your project here when you develop locally.
 $host_dev = 'lending-project';
@@ -16,7 +16,7 @@ $host_dev = 'lending-project';
 // Fill in the domain name here when your project is online.
 // Example : www.johndoe.com
 //           johndoe.com
-$host_prod = 'your_domain.tld';
+$host_prod = 'loans-application.herokuapp.com';
 
 // WARNING: Do not modify the lines below
 $domain = (in_array($_SERVER['HTTP_HOST'], $config['host_dev'], TRUE)) ? $_SERVER['HTTP_HOST'] . '/' . $host_dev : $host_prod;
